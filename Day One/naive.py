475daefaa90157901a284118c42fbbe78448a10d
Chris P <flippedbauxite@gmail.com>
2016-12-02 14:26:20 -0500

    Started planning out the naive approach to Day One, Part One

---
+++ Day One/naive.py
@@ -0,0 +1,4 @@
+# This is a naive approach at figuring out how far away the end point is.
+# It will take the starting point, 0,0, and then parse the input given in
+# input.txt to find the location of Easter Bunny HQ, then will just simply
+# return the sum of the X and Y coordinates, giving us the shortest path to # the destination.
